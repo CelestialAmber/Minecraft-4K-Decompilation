2355cdfea7a6f0c9df74980d250761b0fd37fb9a
Amber Brault <celestialamber1@gmail.com>
2020-11-29 21:53:27 -0500

    Update Minecraft4K.java

--- Minecraft4K.java
+++ Minecraft4K.java
@@ -4,94 +4,100 @@ import java.util.*;
 
 public class Minecraft4K extends Frame implements Runnable {
    private int[] M = new int[32767];
+   int[] blockData = new int[262144];
+   int[] textureData = new int[12288];
+   Random localRandom = new Random();
 
 
 
-   public static void main(String[] args){
-      Minecraft4K m = new Minecraft4K();
-      m.setSize(856, 480);
-      m.setTitle("Minecraft 4K");
-      m.setVisible(true);
-      m.run();
-   }
+    public static void main(String[] args){
+        Minecraft4K m = new Minecraft4K();
+        m.setSize(856, 480);
+        m.setTitle("Minecraft 4K");
+        m.setVisible(true);
+        m.run();
+    }
+
+    public void GenerateWorld(){
+        localRandom.setSeed(18295169L);
+        for (int i = 0; i < 262144; i++) {
+            blockData[i] = ((i / 64 % 64 > 32 + localRandom.nextInt(8)) ? (localRandom.nextInt(8) + 1) : 0);
+        }
+    }
+
+    public void GenerateTextures(){
+        for (int j = 1; j < 16; j++) {
+            int k = 255 - localRandom.nextInt(96);
+            for (int m = 0; m < 48; m++) {
+                for (int n = 0; n < 16; n++) {
+                    int i2 = 9858122;
+                    if (j == 4) {
+                        i2 = 8355711;
+                    }
+                    if (j != 4 || localRandom.nextInt(3) == 0) {
+                        k = 255 - localRandom.nextInt(96);
+                    }
+                    if (j == 1 && m < (n * n * 3 + n * 81 >> 2 & 0x3) + 18) {
+                        i2 = 6990400;
+                    }
+                    else if (j == 1 && m < (n * n * 3 + n * 81 >> 2 & 0x3) + 19) {
+                        k = k * 2 / 3;
+                    }
+                    if (j == 7) {
+                        i2 = 6771249;
+                        if (n > 0 && n < 15 && ((m > 0 && m < 15) || (m > 32 && m < 47))) {
+                            i2 = 12359778;
+                            int i3 = n - 7;
+                            int i4 = (m & 0xF) - 7;
+                            if (i3 < 0) {
+                                i3 = 1 - i3;
+                            }
+                            if (i4 < 0) {
+                                i4 = 1 - i4;
+                            }
+                            if (i4 > i3) {
+                                i3 = i4;
+                            }
+                            k = 196 - localRandom.nextInt(32) + i3 % 3 * 32;
+                        }
+                        else if (localRandom.nextInt(2) == 0) {
+                            k = k * (150 - (n & 0x1) * 100) / 100;
+                        }
+                    }
+                    if (j == 5) {
+                        i2 = 11876885;
+                        if ((n + m / 4 * 4) % 8 == 0 || m % 4 == 0) {
+                            i2 = 12365733;
+                        }
+                    }
+                    int i3 = k;
+                    if (m >= 32) {
+                        i3 /= 2;
+                    }
+                    if (j == 8) {
+                        i2 = 5298487;
+                        if (localRandom.nextInt(2) == 0) {
+                            i2 = 0;
+                            i3 = 255;
+                        }
+                    }
+                    int i4 = (i2 >> 16 & 0xFF) * i3 / 255 << 16 | (i2 >> 8 & 0xFF) * i3 / 255 << 8 | (i2 & 0xFF) * i3 / 255;
+                    textureData[n + m * 16 + j * 256 * 3] = i4;
+                }
+             }
+          }
+    }
 
 
 
    @Override
    public void run() {
       try {
-          final Random localRandom = new Random();
-          final BufferedImage localBufferedImage = new BufferedImage(214, 120, 1);
-          final int[] arrayOfInt1 = ((DataBufferInt)localBufferedImage.getRaster().getDataBuffer()).getData();
-          final int[] arrayOfInt2 = new int[262144];
-          localRandom.setSeed(18295169L);
+          BufferedImage image = new BufferedImage(214, 120, 1);
+          int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 
-          for (int i = 0; i < 262144; i++) {
-              arrayOfInt2[i] = ((i / 64 % 64 > 32 + localRandom.nextInt(8)) ? (localRandom.nextInt(8) + 1) : 0);
-          }
-
-          final int[] arrayOfInt3 = new int[12288];
-
-          for (int j = 1; j < 16; j++) {
-              int k = 255 - localRandom.nextInt(96);
-              for (int m = 0; m < 48; m++) {
-                  for (int n = 0; n < 16; n++) {
-                      int i2 = 9858122;
-                      if (j == 4) {
-                          i2 = 8355711;
-                      }
-                      if (j != 4 || localRandom.nextInt(3) == 0) {
-                          k = 255 - localRandom.nextInt(96);
-                      }
-                      if (j == 1 && m < (n * n * 3 + n * 81 >> 2 & 0x3) + 18) {
-                          i2 = 6990400;
-                      }
-                      else if (j == 1 && m < (n * n * 3 + n * 81 >> 2 & 0x3) + 19) {
-                          k = k * 2 / 3;
-                      }
-                      if (j == 7) {
-                          i2 = 6771249;
-                          if (n > 0 && n < 15 && ((m > 0 && m < 15) || (m > 32 && m < 47))) {
-                              i2 = 12359778;
-                              int i3 = n - 7;
-                              int i4 = (m & 0xF) - 7;
-                              if (i3 < 0) {
-                                  i3 = 1 - i3;
-                              }
-                              if (i4 < 0) {
-                                  i4 = 1 - i4;
-                              }
-                              if (i4 > i3) {
-                                  i3 = i4;
-                              }
-                              k = 196 - localRandom.nextInt(32) + i3 % 3 * 32;
-                          }
-                          else if (localRandom.nextInt(2) == 0) {
-                              k = k * (150 - (n & 0x1) * 100) / 100;
-                          }
-                      }
-                      if (j == 5) {
-                          i2 = 11876885;
-                          if ((n + m / 4 * 4) % 8 == 0 || m % 4 == 0) {
-                              i2 = 12365733;
-                          }
-                      }
-                      int i3 = k;
-                      if (m >= 32) {
-                          i3 /= 2;
-                      }
-                      if (j == 8) {
-                          i2 = 5298487;
-                          if (localRandom.nextInt(2) == 0) {
-                              i2 = 0;
-                              i3 = 255;
-                          }
-                      }
-                      int i4 = (i2 >> 16 & 0xFF) * i3 / 255 << 16 | (i2 >> 8 & 0xFF) * i3 / 255 << 8 | (i2 & 0xFF) * i3 / 255;
-                      arrayOfInt3[n + m * 16 + j * 256 * 3] = i4;
-                  }
-               }
-            }
+          GenerateWorld();
+          GenerateTextures();
 
           float f1 = 96.5f;
           float f2 = 65.0f;
@@ -99,7 +105,7 @@ public class Minecraft4K extends Frame implements Runnable {
           float f4 = 0.0f;
           float f5 = 0.0f;
           float f6 = 0.0f;
-          long l = System.currentTimeMillis();
+          long currentTimeMilliseconds = System.currentTimeMillis();
           int i5 = -1;
           int i6 = 0;
           float f7 = 0.0f;
@@ -113,7 +119,7 @@ public class Minecraft4K extends Frame implements Runnable {
 
           Label_1192:
               while (true) {
-                  while (System.currentTimeMillis() - l > 10L) {
+                  while (System.currentTimeMillis() - currentTimeMilliseconds > 10) {
                       if (this.M[2] > 0) {
                           final float f13 = (this.M[2] - 428) / 214.0f * 2.0f;
                           final float f14 = (this.M[3] - 240) / 120.0f * 2.0f;
@@ -133,7 +139,8 @@ public class Minecraft4K extends Frame implements Runnable {
                           }
                       }
 
-                      l += 10L;
+                      currentTimeMilliseconds += 10;
+
                       float f13 = 0.0f;
                       float f14 = 0.0f;
                       f14 += (this.M[119] - this.M[115]) * 0.02f;
@@ -155,7 +162,7 @@ public class Minecraft4K extends Frame implements Runnable {
                               final int i9 = (int)(f16 + (i8 >> 0 & 0x1) * 0.6f - 0.3f) - 64;
                               final int i10 = (int)(f17 + ((i8 >> 2) - 1) * 0.8f + 0.65f) - 64;
                               final int i11 = (int)(f18 + (i8 >> 1 & 0x1) * 0.6f - 0.3f) - 64;
-                              if (i9 < 0 || i10 < 0 || i11 < 0 || i9 >= 64 || i10 >= 64 || i11 >= 64 || arrayOfInt2[i9 + i10 * 64 + i11 * 4096] > 0) {
+                              if (i9 < 0 || i10 < 0 || i11 < 0 || i9 >= 64 || i10 >= 64 || i11 >= 64 || blockData[i9 + i10 * 64 + i11 * 4096] > 0) {
                                   if (i7 != 1) {
                                       continue Label_1192;
                                   }
@@ -168,7 +175,7 @@ public class Minecraft4K extends Frame implements Runnable {
                                   continue Label_1192;
                               }
                               else {
-                                  ++i8;
+                                  i8++;
                               }
                           }
 
@@ -183,12 +190,12 @@ public class Minecraft4K extends Frame implements Runnable {
               int i13 = 0;
 
               if (this.M[1] > 0 && i5 > 0) {
-                  arrayOfInt2[i5] = 0;
+                  blockData[i5] = 0;
                   this.M[1] = 0;
               }
 
               if (this.M[0] > 0 && i5 > 0) {
-                  arrayOfInt2[i5 + i6] = 1;
+                  blockData[i5 + i6] = 1;
                   this.M[0] = 0;
               }
 
@@ -197,7 +204,7 @@ public class Minecraft4K extends Frame implements Runnable {
                   final int i16 = (int)(f2 + ((i14 >> 2) - 1) * 0.8f + 0.65f) - 64;
                   final int i17 = (int)(f3 + (i14 >> 1 & 0x1) * 0.6f - 0.3f) - 64;
                   if (i15 >= 0 && i16 >= 0 && i17 >= 0 && i15 < 64 && i16 < 64 && i17 < 64) {
-                      arrayOfInt2[i15 + i16 * 64 + i17 * 4096] = 0;
+                      blockData[i15 + i16 * 64 + i17 * 4096] = 0;
                   }
               }
 
@@ -247,13 +254,13 @@ public class Minecraft4K extends Frame implements Runnable {
 
                           if (f27 < 0.0f) {
                               if (i21 == 0) {
-                                  --f34;
+                                  f34--;
                               }
                               if (i21 == 1) {
-                                  --f35;
+                                  f35--;
                               }
                               if (i21 == 2) {
-                                  --f36;
+                                  f36--;
                               }
                           }
 
@@ -270,7 +277,7 @@ public class Minecraft4K extends Frame implements Runnable {
                               }
 
                               final int i25 = i22 + i23 * 64 + i24 * 4096;
-                              final int i26 = arrayOfInt2[i25];
+                              final int i26 = blockData[i25];
 
                               if (i26 > 0) {
                                   i12 = ((int)((f34 + f36) * 16.0f) & 0xF);
@@ -287,7 +294,7 @@ public class Minecraft4K extends Frame implements Runnable {
                                   int i27 = 16777215;
 
                                   if (i25 != i5 || (i12 > 0 && i13 % 16 > 0 && i12 < 15 && i13 % 16 < 15)) {
-                                      i27 = arrayOfInt3[i12 + i13 * 16 + i26 * 256 * 3];
+                                      i27 = textureData[i12 + i13 * 16 + i26 * 256 * 3];
                                   }
 
                                   if (f33 < f26 && i15 == this.M[2] / 4 && i17 == this.M[3] / 4) {
@@ -316,13 +323,13 @@ public class Minecraft4K extends Frame implements Runnable {
                       int i21 = (i19 >> 16 & 0xFF) * i20 / 255;
                       final int i28 = (i19 >> 8 & 0xFF) * i20 / 255;
                       final int i29 = (i19 & 0xFF) * i20 / 255;
-                      arrayOfInt1[i15 + i17 * 214] = (i21 << 16 | i28 << 8 | i29);
+                      pixels[i15 + i17 * 214] = (i21 << 16 | i28 << 8 | i29);
                   }
               }
               i5 = (int)i18;
               Thread.sleep(2L);
               if (!this.isActive()) break;
-              this.getGraphics().drawImage(localBufferedImage, 0, 0, 856, 480, (ImageObserver)null);
+              this.getGraphics().drawImage(image, 0, 0, 856, 480, (ImageObserver)null);
           }
       }
       catch (Exception localException) {}
